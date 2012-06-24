cc557c88a373e965cbb868b6282adcad22b0642b
Eloy Espinaco <eloyesp@gmail.com>
2012-06-24 17:44:00 -0300

    mas funciones
    Algunas funciones con matrices muuuuy aburridas

--- prexamen/main.c
+++ prexamen/main.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "matriz.h"
-
+#define PAUSE while (getchar() != '\n');
 int menu(void);
 char * read_line (char * buf, size_t length);
+void reinicializar_matriz(matriz * const m);
+void cargar_valor_matriz(matriz * const m);
+void leer_valor_matriz(const matriz * const m);
 
 int main (void) {
 	int opcion = 0;
@@ -15,21 +18,74 @@ int main (void) {
 		opcion = menu();
 		switch ( opcion ) {
       case 1 :
-        puts("Mostrar matriz");
+        puts("=================\n"
+             "Mostrar matriz");
         matriz_mostrar(&m);
-        while (getchar() != '\n');
-        
+        PAUSE
+        break;
+      case 2 :
+        puts("=================\n"
+             "Reinicializar matriz");
+        reinicializar_matriz(&m);
+        matriz_mostrar(&m);
+        PAUSE
+        break;
+      case 3 :
+        puts("=================\n"
+             "Cargar valor en Matriz");
+        cargar_valor_matriz(&m);
+        matriz_mostrar(&m);
+        PAUSE
+        break;
+      case 4 :
+        puts("=================\n"
+             "Leer valor de la matriz");
+        leer_valor_matriz(&m);
+        matriz_mostrar(&m);
+        PAUSE
+        break;
 			case 5 : 
         puts ("Bye");
 				break;
 			default :
 				puts("Error");
-        while (getchar() != '\n');
+        PAUSE
 				break;
 		}
 	}
 	
 	return 0;
+}
+
+void reinicializar_matriz(matriz * const m) {
+  char input[20];
+  int  filas, columnas;
+  
+  printf("Ingrese la cantidad de filas (<=20): ");
+  read_line(input, 20);
+  filas = atoi(input);
+  while (filas < 1 || filas > 20) {
+    puts("ERROR (el numero de filas debe estar entre 1 y 20)");
+    printf("Ingrese la cantidad de filas (<=20): ");
+    read_line(input, 20);
+    filas = atoi(input);
+  }
+  printf("Ingrese la cantidad de columnas (<=20): ");
+  read_line(input, 20);
+  columnas = atoi(input);
+  while (columnas < 1 || columnas > 20) {
+    puts("ERROR (el numero de columnas debe estar entre 1 y 20)");
+    printf("Ingrese la cantidad de columnas (<=20): ");
+    read_line(input, 20);
+    columnas = atoi(input);
+  }
+  matriz_inicializar(m, filas, columnas);
+}
+void cargar_valor_matriz(matriz * const m){
+  
+}
+void leer_valor_matriz(const matriz * const m){
+  
 }
 
 
@@ -39,10 +95,13 @@ int menu(void)
   system("cls");
 	
 	printf("Menu (ingrese la opcion deseada)\n"
-    "1 - Matrices\n"
+    "1 - Mostrar matriz\n"
+    "2 - Reinicializar Matriz\n"
+    "3 - Cargar valor en la matriz\n"
+    "4 - Leer valor de la matriz\n"
 		"5 - Salir\n"
 		"\nOpcion: ");
-	read_line(&input, 20);
+	read_line(input, 20);
 	
 	return atoi(input);
 }
--- prexamen/matriz.c
+++ prexamen/matriz.c
@@ -23,3 +23,97 @@ void matriz_mostrar(const matriz * const m) {
     printf(" |\n");
   }
 }
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//
+//#define M 5
+//#define N 8
+//#define MAX_VALUE 8
+//
+//{
+//  int matriz[M][N], i, j, falta;
+//  srand(time(NULL));
+//  
+//  // Cargo la Matriz
+//  for(i=0; i<M; i++) {
+//    for(j=0; j<N; j++) {
+//      matriz[i][j]=rand() % MAX_VALUE;
+//    }
+//  }
+//  
+//  // Muestro la matriz
+//  
+//  puts("La matriz a analizar es:");
+//  for(i=0; i<M; i++) {
+//    printf("  | ");
+//    for(j=0; j<N; j++) {
+//      printf("%2d ", matriz[i][j]);
+//    }
+//    printf(" |\n");
+//  }
+//  
+//  // Existe algún cero en la matriz.
+//  falta = 1;
+//  i = 0;
+//  while (falta && i < M) {
+//    j = 0;
+//    while (falta && j < N) {
+//      if (matriz[i][j] == 0)
+//        falta = 0;
+//      j++;
+//    }
+//    i++;
+//  }
+//  
+//  puts(""); //separador
+//  
+//  if (falta)
+//    puts("No hay ceros en la matriz");
+//  else
+//    puts("Hay ceros en la matriz");
+//  
+//  
+//  // Hay mas de 5 ceros en la matriz.
+//  
+//  falta = 5;
+//  i = 0;
+//  while (falta && i < M) {
+//    j = 0;
+//    while (falta && j < N) {
+//      if (matriz[i][j] == 0)
+//        falta--;
+//      j++;
+//    }
+//    i++;
+//  }
+//  
+//  puts(""); //separador
+//  
+//  if (falta)
+//    puts("No hay cinco (5) ceros en la matriz");
+//  else
+//    puts("Hay, al menos cinco ceros en la matriz");
+//  
+//  // Cuantas veces aparece el número cero.
+//  falta = 0;
+//  for(i=0; i<M; i++) {
+//    for(j=0; j<N; j++) {
+//      if (matriz[i][j] == 0)
+//        falta++;
+//    }
+//  }
+//  printf("\nLa cantidad de ceros es: %d\n", falta);
+//  
+//  
+//  puts ("\nTambien se puede acceder a los datos como un vector grande, haciendo un poco de trampa, de forma que se harian mas facil los ejercicios: ");
+//  
+//  // a ver:
+//  for(i=0; i < M * N; i++)
+//    printf("%d ", matriz[0][i]);
+//  printf("\n");
+//  return 0;
+//}
